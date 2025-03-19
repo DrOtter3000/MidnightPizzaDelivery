4e6d90a514f333f3200cc5fb05641dd2376b759e
DrOtter3000 <info@darknet-online.com>
2025-03-19 19:26:12 +0100

    add health manager

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -2,13 +2,16 @@ extends CharacterBody3D
 
 @onready var camera_3d: Camera3D = $Camera3D
 @onready var character_mover: Node3D = $CharacterMover
+@onready var health_manager: Node3D = $HealthManager
 
 @export var mouse_sensetivity := .15
+
+var dead := false
 
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
+	health_manager.died.connect(kill)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Fullscreen"):
@@ -17,6 +20,9 @@ func _process(delta: float) -> void:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
+	if dead:
+		return
 	
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
 	var move_dir = (transform.basis * Vector3(input_dir.x , 0, input_dir.y)).normalized()
@@ -27,7 +33,15 @@ func _process(delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
+	if dead:
+		return
+	
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= event.relative.x * mouse_sensetivity
 		camera_3d.rotation_degrees.x -= event.relative.y * mouse_sensetivity
 		camera_3d.rotation_degrees.x = clamp(camera_3d.rotation_degrees.x, -90, 90)
+
+
+func kill():
+	dead = true
+	character_mover.set_move_dir(Vector3.ZERO)
