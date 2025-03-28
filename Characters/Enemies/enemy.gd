22cf00855e0d4839220125a3be505b929c194884
DrOtter3000 <info@darknet-online.com>
2025-03-28 20:59:42 +0100

    add Zombie enemy

---
+++ Characters/Enemies/enemy.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+class_name Enemy
+
+@onready var health_manager: Node3D = $HealthManager
+
+
+func _ready() -> void:
+	var hitboxes = find_children("*", "HitBox")
+	for hitbox in hitboxes:
+		hitbox.on_hurt.connect(health_manager.hurt)
+
+
+func hurt(damage_data: DamageData):
+	health_manager.hurt(damage_data)
