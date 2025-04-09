7eb2b2784146d6fe85fc8c421a1abecf0fb88194
DrOtter3000 <info@darknet-online.com>
2025-04-09 16:28:12 +0200

    add enemy melee attack

--- Characters/Enemies/enemy.gd
+++ Characters/Enemies/enemy.gd
@@ -5,6 +5,7 @@ class_name Enemy
 @onready var vision_manager: VisionManager = $VisionManager
 @onready var nearby_enemies_alert_area: Area3D = $NearbyEnemiesAlertArea
 @onready var ai_character_mover: Node3D = $AICharacterMover
+@onready var attack_emitter: BulletEmitter = $AttackEmitter
 @export var animation_player: AnimationPlayer
 
 enum STATES {IDLE, ATTACK, DEAD}
@@ -12,12 +13,20 @@ var cur_state = STATES.IDLE
 
 @onready var player = get_tree().get_first_node_in_group("Player")
 
+@export var attack_range := 2.0
+@export var damage := 1.0
+
+
 
 func _ready() -> void:
 	var hitboxes = find_children("*", "HitBox")
 	for hitbox in hitboxes:
 		hitbox.on_hurt.connect(health_manager.hurt)
 	health_manager.died.connect(set_state.bind(STATES.DEAD))
+	
+	hitboxes.append(self)
+	attack_emitter.set_bodies_to_exclude(hitboxes)
+	attack_emitter.set_damage(damage)
 	
 	set_state(STATES.IDLE)
 
@@ -31,14 +40,24 @@ func _process(delta: float) -> void:
 
 
 func process_idle_state(delta):
-	if vision_manager.can_see_player():
+	if vision_manager.can_see_target(player):
 		alert()
 
 
 func process_attack_state(delta):
-	ai_character_mover.set_facing_dir(player.global_position - global_position)
-	ai_character_mover.move_to_point(player.global_position)
-	animation_player.play("Root|Walk")
+	var attacking = animation_player.current_animation == "Root|Attack"
+	var vec_to_player = player.global_position - global_position
+	
+	if vec_to_player.length() <= attack_range:
+		ai_character_mover.stop_moving()
+		if !attacking and vision_manager.is_facing_target(player):
+			animation_player.play("Root|Attack")
+		elif !attacking:
+			ai_character_mover.set_facing_dir(vec_to_player)
+	elif !attacking:
+		ai_character_mover.set_facing_dir(ai_character_mover.move_dir)
+		ai_character_mover.move_to_point(player.global_position)
+		animation_player.play("Root|Walk")
 
 
 func hurt(damage_data: DamageData):
@@ -70,3 +89,7 @@ func set_state(state: STATES):
 			collision_layer = 0
 			collision_mask = 1
 			ai_character_mover.stop_moving()
+
+
+func do_attack(): # called from animation
+	attack_emitter.fire()
--- Characters/vision_manager.gd
+++ Characters/vision_manager.gd
@@ -5,12 +5,11 @@ class_name VisionManager
 @export var max_sight_range := 10.0
 @export var always_detect_in_range := 2.0
 
-@onready var player = get_tree().get_first_node_in_group("Player")
 @onready var los_ray_cast: RayCast3D = $LOSRayCast
 
 
-func can_see_player():
-	var target_pos = player.global_position + Vector3.UP * 1.5
+func can_see_target(target: Node3D):
+	var target_pos = target.global_position + Vector3.UP * 1.5
 	var dir_to_target = global_position.direction_to(target_pos)
 	var dist_to_target = global_position.distance_to(target_pos)
 	var fwd = -global_transform.basis.z
@@ -32,3 +31,9 @@ func can_see_player():
 	los_ray_cast.enabled = false
 	
 	return has_los
+
+
+func is_facing_target(target: Node3D):
+	var pos = to_local(target.global_position)
+	return pos.z < 0.0 and abs(pos.x) < .5
+	
