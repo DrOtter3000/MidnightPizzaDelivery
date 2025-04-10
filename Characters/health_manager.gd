dd5975a75140f9f9be479c02749ee1efdd589406
DrOtter3000 <info@darknet-online.com>
2025-04-10 16:12:30 +0200

    add blood hit effect

--- Characters/health_manager.gd
+++ Characters/health_manager.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+const BLOOD_HIT_EFFECT = preload("res://Effects/Blood_Effects/blood_hit_effect.tscn")
+
 @export var max_health := 100
 @export var cur_health := max_health
 @export var gib_at := -10
@@ -19,6 +21,8 @@ func _ready() -> void:
 
 
 func hurt(damage_data: DamageData):
+	spawn_bolld_effects(damage_data)
+	
 	if cur_health <= 0:
 		return
 	cur_health -= damage_data.amount
@@ -41,3 +45,9 @@ func heal(amount: int):
 	health_changed.emit(cur_health, max_health)
 	if verbose:
 		print("healed for %s, health: %s/%s" % [amount, cur_health, max_health])
+
+
+func spawn_bolld_effects(damage_data: DamageData):
+	var blood_hit_effect = BLOOD_HIT_EFFECT.instantiate()
+	get_tree().get_root().add_child(blood_hit_effect)
+	blood_hit_effect.global_position = damage_data.hit_pos
